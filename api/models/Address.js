fd8db79e2a298a363ab413d001eb5fc9c3caa625
Joe Wemyss <support@javathenutt.com>
2017-10-04 08:58:24 +0100

    created address model

--- api/models/Address.js
+++ api/models/Address.js
@@ -1,2 +1,17 @@
 const mongoose = require('mongoose');
-const Logger = require('@util/Logger')('ADDRESS_MODEL');
+const Logger   = require('@util/Logger')('ADDRESS_MODEL');
+
+const AddressSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  loc: {
+    type: {type: String, enum: ['Point']},
+    coordinates: {type: [Number], default: [0,0]}
+  }
+});
+AddressSchema.index({loc: '2dsphere'});
+
+const AddressModel = mongoose.model('Address', AddressSchema);
+module.exports = {schema: AddressSchema, model: AddressModel};
