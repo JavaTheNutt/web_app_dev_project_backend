9922ac5b17dc4bcaa104abcc6dd9d80c19224d1e
Joe Wemyss <support@javathenutt.com>
2017-10-04 11:50:08 +0100

    tested email validation

---
+++ api/models/validation/modelValidation.js
@@ -0,0 +1,7 @@
+const Logger = require('@root/util/Logger')('MODEL_VALIDATION');
+const emailValidator = require('email-validator');
+function validateEmail(emailAddress) {
+  Logger.info(`checking if ${emailAddress} is valid`);
+  return emailValidator.validate(emailAddress);
+}
+module.exports = {validateEmail};
---
+++ test/unit/models/validation/modelValidation_test.js
@@ -0,0 +1,48 @@
+require('module-alias/register');
+const expect = require('chai').expect;
+
+const validationService = require('@root/models/validation/modelValidation');
+
+describe('model validation', function () {
+  describe('email validation', function () {
+    it('returns false with incorrect emails', function (done) {
+      let tmpEmail = 'joe';
+      let res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail}" was a correct email`).to.be.false;
+      tmpEmail = 'joe@.com';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail}" was a correct email`).to.be.false;
+      tmpEmail = '@joe.com';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail}" was a correct email`).to.be.false;
+      tmpEmail = '@.com';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail}" was a correct email`).to.be.false;
+      tmpEmail = '@joe.';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail}" was a correct email`).to.be.false;
+      tmpEmail = 'joe@joe.c';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail}" was a correct email`).to.be.false;
+      tmpEmail = 'joe@joe.';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail}" was a correct email`).to.be.false;
+      return done();
+    });
+    it('returns true for valid emails', function (done) {
+      let tmpEmail = 'joewemyss@gmail.com';
+      let res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail} was an invalid email`).to.be.true;
+      tmpEmail = 'joewemyss@gmail.co.uk';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail} was an invalid email`).to.be.true;
+      tmpEmail = 'joe.wemyss@gmail.co.uk';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail} was an invalid email`).to.be.true;
+      tmpEmail = 'joewemyss+social@gmail.co.uk';
+      res      = validationService.validateEmail(tmpEmail);
+      expect(res, `thought "${tmpEmail} was an invalid email`).to.be.true;
+      return done();
+    })
+  })
+})
