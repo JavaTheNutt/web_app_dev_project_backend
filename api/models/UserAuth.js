3a44e169bdf14cb72865ce285b07e0c7a06f7ce3
Joe Wemyss <support@javathenutt.com>
2017-10-05 10:00:12 +0100

    tested for missing values in auth model

--- api/models/UserAuth.js
+++ api/models/UserAuth.js
@@ -2,12 +2,18 @@
  * This Model will represent the authentication details for a User.
  */
 const mongoose = require('mongoose');
-const Logger = require('@util/Logger')('AUTH_MODEL');
+const Logger   = require('@util/Logger')('AUTH_MODEL');
 
 const UserAuthSchema = mongoose.Schema({
-  email: String,
+  email: {
+    type: String,
+    required: true
+  },
   user: mongoose.Schema.ObjectId,
-  firebaseId: String
+  firebaseId: {
+    type: String,
+    required: true
+  }
 }, {collection: 'user_auth'});
 
 const UserAuthModel = mongoose.model('UserAuth', UserAuthSchema);
