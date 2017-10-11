f458352b181990207b92c4bf8d10997f5e50258a
Joe Wemyss <support@javathenutt.com>
2017-10-11 20:41:45 +0100

    basic tests for add user

--- api/components/User/service/userService.js
+++ api/components/User/service/userService.js
@@ -1,7 +1,26 @@
-const Logger   = require('@util/Logger');
+const Logger   = require('@util/Logger')('USER_SERVICE');
+const User = require('@user/models/User').model;
 module.exports = {
-  createUser(userDetails) {
+  async createUser(userDetails) {
     'use strict';
-
+    Logger.info(`request to create user recieved`);
+    Logger.verbose(`details fo user to be created: ${JSON.stringify(userDetails)}`);
+    const newUser = new User(formatDetails(userDetails));
+    Logger.verbose(`created user: ${JSON.stringify(newUser)}`);
+    try{
+      await newUser.save();
+      Logger.verbose(`user saved without error`);
+      return newUser;
+    }catch(err){
+      Logger.warn(`user save failed`);
+      Logger.error(`error: ${err}`);
+      return null;
+    }
+  }
+};
+function formatDetails(details){
+  'use strict';
+  return {
+    email: details.email
   }
 }
--- test/unit/User/service/userService_test.js
+++ test/unit/User/service/userService_test.js
@@ -5,8 +5,26 @@ chai.use(require('sinon-chai'));
 const sinon = require('sinon');
 
 const sandbox = sinon.sandbox.create();
-
+const userService = require('@user/service/userService');
+const User = require('@user/models/User').model;
 describe('user service', ()=>{
   'use strict';
-
-})
+  let userDetails, saveStub, savedResponse;
+  beforeEach(()=>{
+    userDetails = {email: 'test@test.com'};
+    savedResponse = {_id: 'someidhere', email: 'test@test.com'};
+    saveStub = sandbox.stub(User.prototype, 'save');
+  });
+  afterEach(()=>{
+    sandbox.restore();
+  });
+  it('should successfully return a newly created user when passed correct details', async ()=>{
+    const res = await userService.createUser(userDetails);
+    expect(saveStub).to.be.calledOnce;
+    expect(res._doc).to.have.own.keys('_id', 'email', 'addresses');
+    expect(res._doc._id.toString().length).to.equal(24);
+    expect(Array.isArray(res._doc.addresses)).to.be.true;
+    expect(res._doc.addresses.length).to.equal(0);
+    expect(res._doc.email).to.equal(userDetails.email);
+  })
+});
